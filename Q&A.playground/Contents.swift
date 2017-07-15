ead502355c071b6c3dc0708c3b11b84c927220e0
Linh Chu <cnhlinh@gmail.com>
2017-07-15 22:29:16 +1000

    mutating function

--- Q&A.playground/Contents.swift
+++ Q&A.playground/Contents.swift
@@ -5,14 +5,81 @@ import Foundation
 import SpriteKit
 
 
-var listForShort: [Int] = [4, 65, 343, 22, 5445, 343]
-
-let sorted = listForShort.sorted(by: { (s1, s2) in s1 < s2 })
-
-for sort in sorted {
-    print(sort)
+public protocol PriorityQueuable: Hashable {
+    associatedtype KeyType: Comparable
+    associatedtype ValueType: Comparable
+    
+    var key: KeyType { get set }
+    var value: ValueType { get set }
 }
 
+protocol Heap {
+    associatedtype T: Comparable
+    
+    var data: [T] { get set }
+    
+    mutating func heapify(parentIndex: Int)
+}
+
+//protocol PriorityQueue<T> {
+//    associatedtype Item: Heap
+//    
+//    //FIXME: doesn't allow me to do that. Why?
+////    var heap: Heap<Item> { get set }
+//    
+//    // doesn't compile as well
+//    // var heap: Heap { get set }
+//}
+
+
+struct MyClass: Heap {
+    typealias T = Int
+    
+    var data: [T] = []
+    mutating func heapify(parentIndex: Int) {
+        data.append(parentIndex)
+        print(data)
+    }
+}
+
+var a = MyClass()
+a.heapify(parentIndex: 12)
+
+
+
+
+//struct UserInputConfig {
+//}
+//
+//struct UserInput {
+//    enum State {
+//        case unrestricted
+//        case restricted(because: WarningType)
+//        
+//        enum WarningType {
+//            case offline
+//            case forbidden
+//        }
+//    }
+//    
+//    var config: UserInputConfig?
+//    var state: State = .unrestricted
+//    
+//    func isConfigured() -> Bool {
+//        // Arbitrary checks about the config...
+//        return true
+//    }
+//}
+
+
+
+//if case .restricted = userInput.state {
+//    return 1
+//} else if userInput.isConfigured() {
+//    return 1
+//} else {
+//    return 0
+//}
 
 //func enumValues<S: Sequence>(from sequence: S) -> [S.Iterator.Element.RawValue]
 //    where S.Iterator.Element: RawRepresentable {
