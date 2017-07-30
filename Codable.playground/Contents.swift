729777d6950e39d6158ee3e10016142d64b62232
Linh Chu <cnhlinh@gmail.com>
2017-07-30 15:28:11 +1000

    update

--- Codable.playground/Contents.swift
+++ Codable.playground/Contents.swift
@@ -2,62 +2,88 @@
 
 import UIKit
 
-struct Person: Codable {
-    var name: String
-    var history: [Int: String]
-    
-    init() {
-        self.name = "Name"
-        history = [0: "Test"]
-    }
-    
-    init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        self.name = try container.decodeIfPresent(String.self, forKey: .name) ?? "Failed to decode name"
-        self.history = try container.decodeIfPresent([Int: String].self, forKey: .history) ?? [-1: "Failed to decode history"]
-    }
+protocol OrderItem: Codable {
+    var amount:Int{get set}
+    var isPaid:Bool{get set}
 }
 
-let person = Person()
-do {
-    let jsonData = try JSONEncoder().encode(person)
-    
-    if let result = try? JSONDecoder().decode(Person.self, from: jsonData) {
-        print("\(result)\n")
-    }
-    
-    let sampleDictionary: [String: Any] = ["history":[0: "Test"], "name": "Test"]
-    print(sampleDictionary)
-} catch {
-    print(error.localizedDescription)
+struct ProductItem:OrderItem {
+    var amount = 0
+    var isPaid = false
+    var price = 0.0
 }
 
-extension KeyedDecodingContainer {
-    func decodeWrapper<T>(key: K, defaultValue: T) throws -> T
-        where T : Decodable {
-            return try decodeIfPresent(T.self, forKey: key) ?? defaultValue
-    }
+struct Order: Codable {
+    var id:String = ""
+    var sn:String = ""
+    var items:[OrderItem] = []
+    var createdAt:Int64 = 0
+    var updatedAt:Int64 = 0
+    
+//    init(from decoder: Decoder) throws {
+//        let container = try decoder.container(keyedBy: CodingKeys.self)
+//        self.items = try container.decodeIfPresent([OrderItem].self, forKey: .items) ?? []
+//    }
 }
 
 /*********************************/
 
-struct Foo: Codable {
-    var a: Int
-    var b: Int
-    
-    init(a: Int, b: Int) {
-        self.a = a
-        self.b = b
-    }
-    
-    init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        a = try container.decodeIfPresent(Int.self, forKey: .a) ?? 0
-        b = try container.decodeIfPresent(Int.self, forKey: .a) ?? 0
-    }
-}
+//struct Person: Codable {
+//    var name: String
+//    var history: [Int: String]
+//
+//    init() {
+//        self.name = "Name"
+//        history = [0: "Test"]
+//    }
+//
+//    init(from decoder: Decoder) throws {
+//        let container = try decoder.container(keyedBy: CodingKeys.self)
+//        self.name = try container.decodeIfPresent(String.self, forKey: .name) ?? "Failed to decode name"
+//        self.history = try container.decodeIfPresent([Int: String].self, forKey: .history) ?? [-1: "Failed to decode history"]
+//    }
+//}
+//
+//let person = Person()
+//do {
+//    let jsonData = try JSONEncoder().encode(person)
+//
+//    if let result = try? JSONDecoder().decode(Person.self, from: jsonData) {
+//        print("\(result)\n")
+//    }
+//
+//    let sampleDictionary: [String: Any] = ["history":[0: "Test"], "name": "Test"]
+//    print(sampleDictionary)
+//} catch {
+//    print(error.localizedDescription)
+//}
+//
+//extension KeyedDecodingContainer {
+//    func decodeWrapper<T>(key: K, defaultValue: T) throws -> T
+//        where T : Decodable {
+//            return try decodeIfPresent(T.self, forKey: key) ?? defaultValue
+//    }
+//}
 
-let foo = Foo(a: 1, b: 2)
+/*********************************/
+
+//struct Foo: Codable {
+//    var a: Int
+//    var b: Int
+//
+//    init(a: Int, b: Int) {
+//        self.a = a
+//        self.b = b
+//    }
+//
+//    init(from decoder: Decoder) throws {
+//        let container = try decoder.container(keyedBy: CodingKeys.self)
+//        a = try container.decodeIfPresent(Int.self, forKey: .a) ?? 0
+//        b = try container.decodeIfPresent(Int.self, forKey: .a) ?? 0
+//    }
+//}
+//
+//let foo = Foo(a: 1, b: 2)
 //let dict = foo
 
 
--- TypeErasure.playground/Contents.swift
+++ TypeErasure.playground/Contents.swift
@@ -7,50 +7,50 @@ import UIKit
 /**************************************/
 
 // The actual type to use for that associated type isn’t specified until the protocol is adopted
-//protocol Cachable {
-//    associatedtype CacheType
-//    
-//    func decode(_ data: Data) -> CacheType?
-//    func encode() -> Data?
-//}
-//
-//extension String: Cachable {
-//    typealias CacheType = String
-//    func decode(_ data: Data) -> CacheType? {
-//        guard let string = String(data: data, encoding: String.Encoding.utf8) else {
-//            return nil
-//        }
-//        return string
-//    }
-//    func encode() -> Data? {
-//        return data(using: String.Encoding.utf8)
-//    }
-//}
-//
-//class AnyCachable<T>: Cachable {
-//    private let _decode: (_ data: Data) -> T?
-//    private let _encode: () -> Data?
-//    
-//    init<U: Cachable>(_ cachable: U) where U.CacheType == T {
-//        _decode = cachable.decode
-//        _encode = cachable.encode
-//    }
-//    
-//    func decode(_ data: Data) -> T? {
-//        return _decode(data)
-//    }
-//    
-//    func encode() -> Data? {
-//        return _encode()
-//    }
-//}
-//
-//struct CacheItem {
-//    let item: AnyCachable<Any>
-//    let expiryDate: Date
-//}
-//
-//let cache: [AnyCachable<String>] = [AnyCachable("Hello"), AnyCachable("World")]
+protocol Cachable {
+    associatedtype CacheType
+    
+    func decode(_ data: Data) -> CacheType?
+    func encode() -> Data?
+}
+
+extension String: Cachable {
+    typealias CacheType = String
+    func decode(_ data: Data) -> CacheType? {
+        guard let string = String(data: data, encoding: String.Encoding.utf8) else {
+            return nil
+        }
+        return string
+    }
+    func encode() -> Data? {
+        return data(using: String.Encoding.utf8)
+    }
+}
+
+class AnyCachable<T>: Cachable {
+    private let _decode: (_ data: Data) -> T?
+    private let _encode: () -> Data?
+    
+    init<U: Cachable>(_ cachable: U) where U.CacheType == T {
+        _decode = cachable.decode
+        _encode = cachable.encode
+    }
+    
+    func decode(_ data: Data) -> T? {
+        return _decode(data)
+    }
+    
+    func encode() -> Data? {
+        return _encode()
+    }
+}
+
+struct CacheItem {
+    let item: AnyCachable<Any>
+    let expiryDate: Date
+}
+
+let cache: [AnyCachable<String>] = [AnyCachable("Hello"), AnyCachable("World")]
 
 /**************************************/
 /* View Controller */
