da4eab275e78b3f26d755dc37932300dea0aba47
Linh Chu <cnhlinh@gmail.com>
2017-08-01 12:25:17 +1000

    updated to make it work correctly for characters consisting of more than one UTF-16 code point

--- Range.playground/Contents.swift
+++ Range.playground/Contents.swift
@@ -14,40 +14,31 @@ import Foundation
 
 /// Approach 2: specifying Range type
 extension Range where Bound == String.Index {
-    // Add an initializer to Range
     init(_ range: NSRange, in string: String) {
-        let lower = string.index(string.startIndex, offsetBy: range.location)
-        let upper = string.index(string.startIndex, offsetBy: NSMaxRange(range))
-        self.init(uncheckedBounds: (lower: lower, upper: upper))
+        let lower16 = string.utf16.index(string.utf16.startIndex, offsetBy: range.location)
+        let upper16 = string.utf16.index(string.utf16.startIndex, offsetBy: NSMaxRange(range))
+        
+        if let lower = lower16.samePosition(in: string),
+            let upper = upper16.samePosition(in: string) {
+            self.init(lower..<upper)
+        } else {
+            fatalError("init(range:in:) could not be implemented")
+        }
     }
 }
 
-let string = "Swift"
-let range = NSRange(location: 1, length: 3)
+let string = "❄️ Let it snow! ☃️"
 
-let r = Range<String.Index>(range, in: string)
-print(string[r])
+let range1 = NSRange(location: 0, length: 2)
+let r1 = Range<String.Index>(range1, in: string) // ❄️
+
+let range2 = NSRange(location: 1, length: 3)
+let r2 = Range<String.Index>(range2, in: string) // fatal error: init(range:in:) could not be implemented
 
 
 /// Description: Returns the position in the given string that corresponds exactly to this index. This index must be a valid index of characters.utf16. This example first finds the position of a space (UTF-16 code point 32) in a string’s utf16 view and then uses this method find the same position in the string.
 //func samePosition(in characters: String) -> String.Index? {
 //    
-//}
-
-
-
-//extension String.Index: Comparable {
-//    // MARK: Comparable
-//    
-//    static func <(lhs: String.Index, rhs: String.Index) -> Bool {
-//        return lhs < rhs
-//    }
-//    
-//    // MARK: Equatable
-//    
-//    static func ==(lhs: String.Index, rhs: String.Index) -> Bool {
-//        return lhs == rhs
-//    }
 //}
 
 
