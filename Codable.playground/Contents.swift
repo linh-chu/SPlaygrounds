d2df095a0ccba59543244a3657d561dc9d9c6b84
Linh Chu <linhcn@philology.com.au>
2017-10-15 12:56:00 +1100

    add Composing Classes and Protocols

--- Codable.playground/Contents.swift
+++ Codable.playground/Contents.swift
@@ -21,8 +21,8 @@ struct Order: Codable {
     var updatedAt:Int64 = 0
     
     init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        self.items = try container.decodeIfPresent([OrderItem].self, forKey: .items) ?? []
+//        let container = try decoder.container(keyedBy: CodingKeys.self)
+//        self.items = try container.decodeIfPresent([OrderItem].self, forKey: .items) ?? []
     }
     
     func encode(to encoder: Encoder) throws {
--- S4Q&A.playground/Contents.swift
+++ S4Q&A.playground/Contents.swift
@@ -1,64 +1,14 @@
 import UIKit
 
-let array1: Set = Set(1...99).filter({$0 % 2 == 1})
-let array2: Array = Set(1...99).filter({$0 % 2 == 1})
-print(array1)
-print(array2)
+// Composing Classes and Protocols
 
-//class Character {
-//    var name: String
-//    var owner: String
-//
-//    init() {
-//        name = "Frodo"
-//        owner = "Ben"
-//    }
-//
-//    init(name: String, owner: String) {
-//        self.name = name
-//        self.owner = owner
-//    }
-//
-//    func random() -> Character {
-//        return Character(name: "Gollum", owner: "Hans")
-//    }
-//}
-//
-//var characterArray: [Character] = []
-//
-//let testchar = Character()
-//characterArray.append(testchar)
-//let random = testchar.random()
-//characterArray.append(random)
-//
-//for character in characterArray {
-//    print(character.name)
-//}
-
-
-//struct MyCharacter {
-//    var name: String
-//    var owner: String
-//
-//    init() {
-//        name = "Frodo"
-//        owner = "Ben"
-//    }
-//
-//    mutating func random() {
-//        name = "Gollum"
-//        owner = "Hans"
-//    }
-//}
-//
-//var myArray: [MyCharacter] = []
-//
-//var csVal = MyCharacter()
-//myArray.append(csVal)
-//csVal.random()
-//myArray.append(csVal)
-//
-//for character in myArray {
-//    print(character.name)
-//}
-
+protocol Shakeable {
+    func shake()
+}
+extension UIButton: Shakeable { func shake() {} }
+extension UISlider: Shakeable { func shake() {} }
+func shakeEm(controls: [UIColor & Shakeable]) {
+    for control in controls where control.ciColor == UIColor.red {
+        control.shake()
+    }
+}
